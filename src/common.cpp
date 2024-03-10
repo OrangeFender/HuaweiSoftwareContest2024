182cc65fe4a0cc72cd9fdc2b202b9b976ad412cc
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-10 10:46:00 +0800

    Update common.cpp

--- src/common.cpp
+++ src/common.cpp
@@ -1,3 +1,20 @@
+#include"defs.h"
 #include"common.h"
 
 point::point(int x, int y) : x(x), y(y) {}
+
+point point::moveAstep(int direction) {
+    switch (direction) {
+    case RIGHT:
+        return point(x, y + 1);
+    case LEFT:
+        return point(x, y + 1 );
+    case UP:
+        return point(x + 1, y);
+    case DOWN:
+        return point(x - 1, y);
+    default:
+        return point(x, y);
+    }
+}
+
