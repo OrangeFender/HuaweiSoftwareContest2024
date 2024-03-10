1af53ac83154c237889c9f2c2acf20a40cfa6a82
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-10 12:03:47 +0800

    Update common.cpp

--- src/common.cpp
+++ src/common.cpp
@@ -1,5 +1,6 @@
 #include"defs.h"
 #include"common.h"
+#include<cmath>
 
 point::point(int x, int y) : x(x), y(y) {}
 
@@ -18,3 +19,25 @@ point point::moveAstep(int direction) {
     }
 }
 
+point point::moveSteps(int direction, int len) {
+    switch (direction) {
+    case RIGHT:
+        return point(x, y + len);
+    case LEFT:
+        return point(x, y - len);
+    case UP:
+        return point(x + len, y);
+    case DOWN:
+        return point(x - len, y);
+    default:
+        return point(x, y);
+    }
+}
+
+int point::getDistance(point other) {
+    return abs(x - other.x) + abs(y - other.y);
+}
+
+bool point::horizontal_or_vertical(point other) {
+    return x == other.x || y == other.y;
+}
