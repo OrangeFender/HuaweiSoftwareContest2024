2ecd0d571935d88102d948a5e188d65f97ff0cbf
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-10 12:03:37 +0800

    Update graph.hpp

--- src/graph.hpp
+++ src/graph.hpp
@@ -9,9 +9,10 @@ struct Edge
     int distance; 
     int direction; // 方向
     point boundaryStart; // 边界起点，按靠右行出去的边界
+    point boundaryEnd; // 边界终点
     int width; // 宽度
     //bool locked; // 是否被锁定
-    Edge(int destInd, int distance, int direction, point boundaryStart, int width);
+    Edge(int destInd, int distance, int direction, point boundaryStart, point boundaryEnd , int width);
 
 };
 
@@ -24,5 +25,15 @@ public:
     int ylength; // y length of the rectangle
 public:
     Vertex(point start, int xlength, int ylength);
-    void addEdge(int destInd, int distance, int direction, point boundaryStart, int width);
+    void addEdge(int destInd, int distance, int direction, point boundaryStart, point boundaryEnd);
+};
+
+class Graph {
+private:
+    int indexMap[MAP_SIZE_X][MAP_SIZE_Y];// 用于索引坐标对应的顶点
+    std::vector<Vertex> vertices; // Vector to store vertices
+public:
+    int getVertexIndex(point p);
+    int addVertex(point start, int xlength, int ylength);
+    int cutRectangle();
 };
