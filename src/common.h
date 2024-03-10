5cd650dc2327d78443eb1736a30d5ff40e8cd390
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-10 12:03:45 +0800

    Update common.h

--- src/common.h
+++ src/common.h
@@ -3,4 +3,7 @@ struct point {
     int y;
     point(int x, int y);
     point moveAstep(int direction);
+    point moveSteps(int direction, int len);
+    int getDistance(point other);
+    bool horizontal_or_vertical(point other);
 };
