cdfb20ea7b55542eb3c7b6eca84ef7631ee4058f
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-10 23:19:06 +0800

    Delete common.h

--- src/common.h
+++
@@ -1,9 +0,0 @@
-struct point {
-    int x;
-    int y;
-    point(int x, int y);
-    point moveAstep(int direction);
-    point moveSteps(int direction, int len);
-    int getDistance(point other);
-    bool horizontal_or_vertical(point other);
-};
