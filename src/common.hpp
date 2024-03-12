1aa4c44d84bca730c16939ed1313a343b5ad7d49
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-12 15:53:34 +0800

    Update common.hpp

--- src/common.hpp
+++ src/common.hpp
@@ -1,11 +1,31 @@
 #include<cmath>
 
+
+#define INF 1000000000
+
+#define NUM_ROBOTS 10
+#define NUM_BOATS 10
+#define NUM_DOCKS 5
+#define MAP_SIZE_X 200
+#define MAP_SIZE_Y 200
+
+
+enum Direction {
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN
+};
+
+Direction rotateLeft(Direction direction);
+Direction rotateRight(Direction direction);
+
 struct point {
     int x;
     int y;
     point(int x = -1 , int y = -1);
-    point moveOneStep(int direction);
-    point moveSteps(int direction, int len);
+    point moveOneStep(Direction direction);
+    point moveSteps(Direction direction, int len);
     int getDistance(const point& other);
     bool isHorizontalOrVertical(const point& other);
     bool valid();
