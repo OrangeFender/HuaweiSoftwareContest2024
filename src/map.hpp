6f00b0e9dd4455807860aeb089e90c540154cc45
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-16 11:05:37 +0800

    Update map.hpp

--- src/map.hpp
+++ src/map.hpp
@@ -1,14 +1,16 @@
 #include "common.hpp"
-#include "defs.h"
-
+#include "units.hpp"
+#include<vector>
 
 class mapinfo{
-    point dockpoint[NUM_DOCKS];//码头位置
-    bool clearing[MAP_SIZE_X][MAP_SIZE_Y];
-    int distances[NUM_DOCKS][MAP_SIZE_X][MAP_SIZE_Y];
-    void dfs(point p, int dockIndex, int distance );
-    void bfs(int dockIndex);
+    int nowTime;
+    dock docks[NUM_DOCKS];
+
+    bool clearing[MAP_SIZE_X][MAP_SIZE_Y];//表示可以行走
+    //int distances[NUM_DOCKS][MAP_SIZE_X][MAP_SIZE_Y];
+    void dfs(point src, int distance,int distances[MAP_SIZE_X][MAP_SIZE_Y] );
+    void bfs(point src,int distances[MAP_SIZE_X][MAP_SIZE_Y]);
     public:
     mapinfo(char map[MAP_SIZE_X][MAP_SIZE_Y]);//初始化码头位置和每个点的距离
-    int getDistance(point p, int dockIndex);
+    //int getDistance(point p, int dockIndex);
 };
