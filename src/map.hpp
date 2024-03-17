83e19393add75127c0cf4dc73fe79cde1bd654c2
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-18 06:25:18 +0800

    Update map.hpp

--- src/map.hpp
+++ src/map.hpp
@@ -5,7 +5,7 @@
 class mapinfo{
     int nowTime;
     dock docks[NUM_DOCKS];
-
+    
     bool clearing[MAP_SIZE_X][MAP_SIZE_Y];//表示可以行走
     //int distances[NUM_DOCKS][MAP_SIZE_X][MAP_SIZE_Y];
     void dfs(point src, int distance,int distances[MAP_SIZE_X][MAP_SIZE_Y] );
