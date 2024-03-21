8ecfa3d0e565ee30af6101e501e80510703e230b
Xingjian Tian <139055388+TTT-08@users.noreply.github.com>
2024-03-21 23:10:47 +0800

    Update map.hpp

--- src/map.hpp
+++ src/map.hpp
@@ -8,13 +8,12 @@ class mapinfo{
     int nowTime;
     bool clearing[MAP_SIZE_X][MAP_SIZE_Y];//表示可以行走
     //int distances[NUM_DOCKS][MAP_SIZE_X][MAP_SIZE_Y];
-    char** map;
     
 public:
     
     void dfs(point src, int distance,int distances[MAP_SIZE_X][MAP_SIZE_Y] );
     void bfs(point src,int distances[MAP_SIZE_X][MAP_SIZE_Y]);
-    mapinfo(char** map);//初始化码头位置和每个点的距离
+    mapinfo(char m[MAP_SIZE_X+5][MAP_SIZE_Y+5]);//初始化码头位置和每个点的距离
     //int getDistance(point p, int dockIndex);
     //void setBots(robot* bots);
 };
