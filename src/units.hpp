eff573c4d9d13c43b44f41dbec9d136b2449fbfe
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-21 11:53:26 +0800

    Update units.hpp

--- src/units.hpp
+++ src/units.hpp
@@ -1,4 +1,5 @@
 #include"common.hpp"
+#include"map.hpp"
 //关于机器人的状态
 #define FETCH 0
 #define RETURN 1
@@ -6,11 +7,16 @@
 
 #include<list>
 
+#define BOAT_BACK 0
+#define BOAT_GO 1
+#define BOAT_LOADING 2
 
 struct boat{
     int setoffTime;//出发时间，第几帧
-    int capacity;
+    static int capacity;
     int whichDock;
+    int status;
+    boat();
 };
 
 struct box{
@@ -20,22 +26,25 @@ struct box{
     box();
 };
 
+
 struct dock{
     const int threshold1 = 50;
     const int threshold2 = 20;
 
     int id;
-    int vLoad;
+    int transport_time;
+    int loading_speed;
     int vRobot;
-    robots* robot;
+    int RobotID;
     point position;
     int distances[MAP_SIZE_X][MAP_SIZE_Y];
     int counter;//物品数量
     dock();
+    dock(mapinfo& M, point p, int id,int Load);//初始化码头位置和每个点的距离
     void calcVRobot();
 };
 
-struct robots{
+struct robot{
     const int bias = 5;//越大越倾向于价值大的箱子
     int id;
     int Dockid;
@@ -48,12 +57,15 @@ struct robots{
     bool pull;
     bool get;
     int modified;
+    robot();
+    robot(point p,int id);
     void initPerFrame(point p);
     void findBestBox(std::list<box>& boxes, int currentTime);
     void greedyGetNext();
     bool pullBox();
     bool getBox();
     void Reset();//以防箱子丢失
-    void findCollision(robots others[], int size);
-    void handleCollision(robots& other, int flag);
+    void findCollision(robot others[], int size);
+    void handleCollision(robot& other, int flag);
+    void findBestDock(dock docks[], int size);
 };
