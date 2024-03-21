7b5187fe93c11d564388067ad1646af8dca58b75
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-21 11:53:34 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,41 @@
 #include<iostream>
+
 #include"units.hpp"
-void init(){
 
-}
+#include"common.hpp"
 
+char map[MAP_SIZE_X][MAP_SIZE_Y];
 
+dock docks[NUM_DOCKS] = {};
+robot robots[NUM_ROBOTS] = {};
+mapinfo M;
 
+char okk[100] = {};
 int main(){
-    init();//初始化
-    while(1){
-        //读入数据
+    for(int i = 0; i < MAP_SIZE_X; i++){
+        for(int j = 0; j < MAP_SIZE_Y; j++){
+            scanf("%c", &map[i][j]);
+        }
+    }
+    for(int i = 0; i < NUM_DOCKS; i++){
+        int x, y, s, v;
+        scanf("%d %d %d", &x, &y, &s, &v);
+        docks[i].position = point(x, y);
+        docks[i].transport_time = s;
+        docks[i].loading_speed = v;
+    }
+    
+    mapinfo M((char**)map);
+    M.setBots(robots);
+    //匹配机器人和港口
+    
+    scanf("%s", okk);
+    printf("OK\n");
+    fflush(stdout);
 
+    while(1){
+
+        //读入数据
         //处理数据
         //处理新物品,bfs
         //判断机器人是否在码头
