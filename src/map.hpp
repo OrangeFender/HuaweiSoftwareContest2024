3f54deb640f068380b1f299a3965118871e2bd24
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-21 11:53:30 +0800

    Update map.hpp

--- src/map.hpp
+++ src/map.hpp
@@ -1,17 +1,20 @@
 #include "common.hpp"
-#include "units.hpp"
 #include<vector>
+
+
+class robot;
 
 class mapinfo{
     int nowTime;
-    dock docks[NUM_DOCKS];
-    
     bool clearing[MAP_SIZE_X][MAP_SIZE_Y];//表示可以行走
     //int distances[NUM_DOCKS][MAP_SIZE_X][MAP_SIZE_Y];
+    char** map;
+    
+    public:
+    
     void dfs(point src, int distance,int distances[MAP_SIZE_X][MAP_SIZE_Y] );
     void bfs(point src,int distances[MAP_SIZE_X][MAP_SIZE_Y]);
-    public:
-    mapinfo(char map[MAP_SIZE_X][MAP_SIZE_Y]);//初始化码头位置和每个点的距离
+    mapinfo(char** map);//初始化码头位置和每个点的距离
     //int getDistance(point p, int dockIndex);
-    
+    void setBots(robot* bots);
 };
