710949d5c3fafc9d2aeae8be284e1ab0d7581a7f
Xingjian Tian <139055388+TTT-08@users.noreply.github.com>
2024-03-21 23:10:51 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@
 
 #include"common.hpp"
 
-char map[MAP_SIZE_X][MAP_SIZE_Y];
+#include <fstream>
+
+
+char m[MAP_SIZE_X+5][MAP_SIZE_Y+5];
 
 dock docks[NUM_DOCKS] = {};
 robot robots[NUM_ROBOTS] = {};
@@ -16,25 +19,32 @@ char okk[100] = {};
 std::list<box> boxes;
 
 int main(){
+    
+    std::ofstream debugFile("debug.txt");
+
     for(int i=0;i<NUM_ROBOTS;i++){
         robots[i].id = i;
     }
-    for(int j = 0; j < MAP_SIZE_X; j++){
-        for(int i = 0; i < MAP_SIZE_Y; i++){
-            scanf("%c", &map[i][j]);
-        }
+    for(int i = 0; i < MAP_SIZE_X; i++){
+        scanf("%s", m[i]);
     }
     
-    mapinfo M((char**)map);
+    mapinfo M(m);
 
     for(int i = 0; i < NUM_DOCKS; i++){
-        scanf("%d",& docks[i].id);
+        scanf("%d",&docks[i].id);
         int x, y, s, v;
         scanf("%d %d %d %d", &x, &y, &s, &v);
         docks[i].position = point(x, y);
         docks[i].transport_time = s;
         docks[i].loading_speed = v;
         docks[i].setDistance(M);
+        // for(int ii=0;ii<MAP_SIZE_X;ii++){
+        //     for(int jj=0;jj<MAP_SIZE_Y;jj++){
+        //         std::cout<<docks[i].distances[ii][jj]<<" ";
+        //     }
+        //     std::cout<<std::endl;
+        // }
     }
     
     scanf("%d",&boats[0].capacity);
@@ -53,22 +63,32 @@ int main(){
             int x, y, val;
             scanf("%d %d %d", &x, &y, &val);
             box b(point(x, y), val, realframe);
-            boxes.push_back(b);
+            boxes.push_back(b);//将箱子加入箱子列表
         }
         for(int i = 0; i < NUM_ROBOTS; i++){
-            int goods, x, y, sts;
-            scanf("%d %d %d %d", &goods, &x, &y, &sts);
-            robots[i].initPerFrame(point(x, y));
+            int have, x, y, sts;
+            scanf("%d %d %d %d", &have, &x, &y, &sts);
+            //debugFile<<"id"<<i<<" x:"<<x<<" y:"<<y<<std::endl;
+            robots[i].initPerFrame((bool) have,point(x, y));
+            //debugFile<<"id"<<i<<" x:"<<robots[i].position.x<<" y:"<<robots[i].position.y<<std::endl;
             //robots[i].status = sts;
             if (frame==1){//第一帧匹配港口
                 robots[i].findBestDock(docks, NUM_DOCKS);
+                std::cerr<<"dock "<<robots[i].targetDock->id<<std::endl;
+                // for(int ii=0;ii<MAP_SIZE_X;ii++){
+                //     for(int jj=0;jj<MAP_SIZE_Y;jj++){
+                //         std::cerr<<robots[i].targetDock->distances[ii][jj]<<" ";
+                //     }
+                //     std::cerr<<std::endl;
+                // }
             }
         }
         for(int i = 0; i < NUM_BOATS; i++){
             int sts, pos;
-            scanf("%d %d", &sts, &pos);
+            scanf("%d %d\n", &sts, &pos);
             //
         }
+        scanf("%s", okk);
         //处理数据
         for(int i = 0; i < NUM_ROBOTS; i++){
             if(robots[i].status == FETCH){
@@ -78,10 +98,23 @@ int main(){
                 robots[i].pullBox();
             }
             if(robots[i].status == PENDING){
-                robots[i].findBestBox(boxes, frame);
+                debugFile<<"boxsize"<<boxes.size()<<std::endl;
+                robots[i].findBestBox(boxes, realframe,M);
             }
             robots[i].greedyGetNext();
+            
+            if(robots[i].status == FETCH){
+                debugFile<<"boxpos"<<robots[i].targetBox.position.x<<" "<<robots[i].targetBox.position.y<<std::endl;
+                debugFile<<"id"<<i<<" x:"<<robots[i].position.x<<" y:"<<robots[i].position.y<<" Dis:"<<robots[i].position.getMapValue(robots[i].boxBFS)<<" dir:"<<robots[i].next<<" nextDis:"<<robots[i].position.moveOneStep(robots[i].next).getMapValue(robots[i].boxBFS)<<std::endl;        
+            }
+            //debugFile<<"id"<<i<<" x:"<<robots[i].position.x<<" y:"<<robots[i].position.y<<" Dis:"<<robots[i].position.getMapValue(robots[i].targetDock->distances)<<" dir:"<<robots[i].next<<" nextDis:"<<robots[i].position.moveOneStep(robots[i].next).getMapValue(robots[i].targetDock->distances)<<std::endl;
         }
+        
+        for(int i = 0; i < NUM_BOATS; i++){
+            robots[i].findCollision(robots, NUM_ROBOTS);
+        }
+
+
         //输出数据
         for(int i = 0; i < NUM_ROBOTS; i++){
             if(robots[i].pull){
@@ -98,7 +131,7 @@ int main(){
 
 
 
-        puts("OK");
+        printf("OK\n");
         fflush(stdout);
     }
     return 0;
