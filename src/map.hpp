62a7e5f501abf8567e0dead37bd3f98b557099a3
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2024-03-23 14:47:09 +0800

    Update map.hpp

--- src/map.hpp
+++ src/map.hpp
@@ -15,6 +15,7 @@ public:
     void dfs(point src, int distance,int distances[MAP_SIZE_X][MAP_SIZE_Y] );
     void bfs(point src,int distances[MAP_SIZE_X][MAP_SIZE_Y]);
     mapinfo(char m[MAP_SIZE_X+5][MAP_SIZE_Y+5]);//初始化码头位置和每个点的距离
+    int degree(point p);//计算一个点的度数
     //int getDistance(point p, int dockIndex);
     //void setBots(robot* bots);
 };
